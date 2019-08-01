21fc1fe94f52bfd76fe749d58983bd9b8fb6d5c4
Eshwar0601 <40708201+Eshwar0601@users.noreply.github.com>
2019-08-01 19:29:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-function preview(x) {
-    console.log(x)
+function preview(num) {
+    document.form.textView.value = document.form.textView.value+num;
+}
+
+function solve() {
+	document.form.textView.value = eval(document.form.textView.value);
+}
+
+
+function del() {
+	document.form.textView.value = '';
 }
